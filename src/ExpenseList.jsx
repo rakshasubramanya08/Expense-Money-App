b2945c9015ca7d350be068a4b99219d52bcad720
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-09-24 17:05:17 +0200

    Added new functions

--- src/ExpenseList.jsx
+++ src/ExpenseList.jsx
@@ -1,35 +1,50 @@
 import React from 'react'
 import './ExpenseList.css'
 
-function ExpenseList() {
+function ExpenseList({ expenses, onDelete }) {
+  const total = expenses.reduce((sum, exp) => sum + exp.amount, 0);
+
   return (
     <>
-    <div className='tableBox'>
-      <table className='table'>
-        <tr>
-           <th>Sl.no</th>
-           <th>Expense Name</th> 
-           <th>Expense Amount</th>
-           <th>Delete</th>     
-        </tr>
+      <div className='tableBox'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Sl.no</th>
+              <th>Expense Name</th>
+              <th>Expense Amount</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.length === 0 ? (
+              <tr>
+                <td colSpan="4">No expenses added yet</td>
+              </tr>
+            ) : (
+              expenses.map((exp, index) => (
+                <tr key={exp.id}>
+                  <td>{index + 1}</td>
+                  <td>{exp.name}</td>
+                  <td>{exp.amount}</td>
+                  <td>
+                    <button 
+                      className='delbtn' 
+                      onClick={() => onDelete(exp.id)}
+                    >
+                      ⛔
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
 
-        <tr>
-            <td>1</td>
-            <td>Petrol</td>
-            <td>2000</td>
-            <td><button className='delbtn'>⛔</button></td>  
-        </tr>  
-        <tr>
-            <td>2</td>
-            <td>Food</td>
-            <td>1500</td>
-            <td><button className='delbtn'>⛔</button></td>  
-        </tr>        
-     </table>
-    </div>
-    <div className="total">
-        <h3>Total Expense Amount : 3500 </h3>
-    </div>
+      <div className="total">
+        <h3>Total Expense Amount : {total} </h3>
+      </div>
     </>
   )
 }
