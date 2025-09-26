328b3f8bcfc66aaebfdae9ca26c53e37392a8e2b
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-09-26 12:47:03 +0200

    modified.

--- src/backend/models/Expense.js
+++ src/backend/models/Expense.js
@@ -1,19 +1,9 @@
 const mongoose = require("mongoose");
 
 const expenseSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  amount: {
-    type: Number,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+  name: { type: String, required: true, trim: true },
+  amount: { type: Number, required: true },
+  date: { type: Date, default: Date.now }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Expense", expenseSchema);
