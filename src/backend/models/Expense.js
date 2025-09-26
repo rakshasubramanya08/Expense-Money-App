c686e5084b6f099362069c3bce7c9392c413e1b6
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-09-26 12:41:02 +0200

    Created Mongoose file.

---
+++ src/backend/models/Expense.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Expense", expenseSchema);
